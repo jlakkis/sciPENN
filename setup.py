89822cdeae5f535d65dbda4ebdd8314904278393
Justin Lakkis <jlakkis@Justins-MacBook-Pro.local>
2021-09-01 07:08:49 -0400

    fix github link

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="sciPENN",
-    version="0.9.2",
+    version="0.9.3",
     author="Justin Lakkis",
     author_email="jlakks@gmail.com",
     description="A package for integrative and predictive analysis of CITE-seq data",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/jlakkis/sciPENN",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
